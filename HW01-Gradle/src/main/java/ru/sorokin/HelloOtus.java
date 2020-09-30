24de80901b6713c0ec8163800a7b3faf154f9662
Valery Sorokin <sorvall@mail.ru>
2020-09-30 21:24:43 +0300

    HW01

---
+++ HW01-Gradle/src/main/java/ru/sorokin/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.sorokin;
+
+import com.google.common.collect.*;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(Lists.newArrayList(1, 2, 3));
+    }
+}
